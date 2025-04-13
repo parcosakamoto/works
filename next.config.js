47c597d5f59c84d5711c1d2cf01f3d0b81c537b4
parcosakamoto <parco_vita@icloud.com>
2025-04-13 13:54:40 +0900

    アプデ修正

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ import withExportImages from "next-export-optimize-images";
 /** @type {import('next').NextConfig} */
 const config = withExportImages({
   trailingSlash: true,
+  //output: "export", // Static export を有効化
   // 必要であれば他の Next.js 設定をここに追加
 });
 
