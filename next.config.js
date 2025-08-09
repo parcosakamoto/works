a8d33f52313d7eabc44b6e6d9d8b86de1a7c87cd
parcosakamoto <parco_vita@icloud.com>
2025-08-09 13:40:05 +0900

    back

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const config = withExportImages({
   trailingSlash: true,
   //output: "export", // Static export を有効化
   // 必要であれば他の Next.js 設定をここに追加
+  images: {
+    loader: "default",
+  },
 });
 
 export default config;
